67fa34085bfa1f8d8cbc15aa6137556f73b5bdfa
sivaprasad2 <sivann272001@gmail.com>
2023-04-21 10:12:53 +0530

    frontend

--- src/components/Schemes/Schemes.jsx
+++ src/components/Schemes/Schemes.jsx
@@ -1,9 +1,53 @@
 import React from 'react'
+const links = [
+  {
+    id: 1,
+    title: 'Food Scheme',
+    desc: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptate pariatur',
+    lk: "https://www.mofpi.gov.in/",
+  },
+  {
+    id: 2,
+    title: 'Education Scheme',
+    desc: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptate pariatu ',
+    lk: "https://www.mofpi.gov.in/",
 
-function schemes() {
+  },
+  {
+    id: 3,
+    title: 'Clothings',
+    desc: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptate pariatu',
+    lk: "https://www.mofpi.gov.in/",
+  },
+  {
+    id: 4,
+    title: 'Clothings',
+    desc: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptate pariatu',
+    lk: "https://www.mofpi.gov.in/",
+  },
+]
+
+const handleClick = (link) => {
+  window.open("https://www.mofpi.gov.in/");
+};
+
+function Schemes() {
   return (
-    <div>schemes</div>
+    <div className='links-container'>
+      <div href={links.lk} className="scheme-card">
+        {links.map((item) => (
+          <div key={item.id} className='scheme-box' >
+            <div className="scheme-title">
+              {item.title}
+            </div>
+            <div className="scheme-desc">
+              {item.desc}
+            </div>
+            <a href={item.lk}>Click</a>
+          </div>
+        ))}
+      </div>
+    </div>
   )
 }
-
-export default schemes
+export default Schemes
